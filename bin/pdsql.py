aeb0c63e2a12e07076d9b72593a978520ca7fc82
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-04-18 19:21:40 -0400

    pdsql

---
+++ bin/pdsql.py
@@ -0,0 +1,58 @@
+import argparse
+
+import fastparquet as fp
+import pandas as pd
+from pandasql import sqldf
+
+
+def pysqldf(q):
+    return sqldf(q, globals())
+
+
+def get_file(name, file_format):
+    if file_format == "parquet":
+        return fp.ParquetFile(name).to_pandas()
+    elif file_format == "csv":
+        return pd.read_csv(name)
+    else:
+        raise ValueError("unknown file format: {}".format(file_format))
+
+
+def save_file(df, name, file_format):
+    if file_format == "parquet":
+        fp.write(name, df)
+    elif file_format == "csv":
+        df.to_csv(name, index=False)
+    else:
+        raise ValueError("unknown file format: {}".format(file_format))
+
+
+parser = argparse.ArgumentParser(description='Query a structured file using SQL syntax')
+parser.add_argument('input', nargs='+', help='Input files')
+parser.add_argument('--sql', '-q', help='SQL query')
+parser.add_argument('--output', '-o', help='Output file')
+parser.add_argument('--input-formats', '-f', nargs='*', help='A list of input file formats')
+parser.add_argument('--output-format', '-F', help='The output file format', default="parquet")
+args = parser.parse_args()
+
+formats = args.input_formats
+if formats is None:
+    formats = ["parquet" for i in args.input]
+if len(formats) == 1 and len(args.input) > 1:
+    formats = [formats[0] for i in args.input]
+if len(formats) < len(args.input):
+    raise ValueError("Not enough input formats")
+
+for idx, file in enumerate(args.input):
+    vars()["df{}".format(idx)] = get_file(file, formats[idx])
+df = vars()["df0"]
+
+if args.sql is None:
+    r = df
+else:
+    r = pysqldf(args.sql)
+
+if args.output is None:
+    print(r)
+else:
+    save_file(r, args.output, args.output_format)
